941b2497b14f027813ea3898fdd2761a83b1349a
Oleh Medvid <meandjob@protonmail.com>
2023-03-04 23:47:57 +0200

    update: vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { join } from "path";
+
 
 export default defineConfig({
   plugins: [react()],
   base: "/test-task-tweets/",
-  build: {
-    outDir: "dist",
-  },
-  esbuild: {
-    jsxFactory: "h",
-    jsxFragment: "Fragment",
-  },
-  optimizeDeps: {
-    include: ["preact/hooks"],
-  },
-
-  resolve: {
-    alias: {
-      "/@": join(__dirname, "src"),
-    },
-  },
+ 
 });
