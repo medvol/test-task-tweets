bd2c19e7e00e7782c7132377689803b4439a4a3f
Oleh Medvid <meandjob@protonmail.com>
2023-03-04 23:18:37 +0200

    add:vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { join } from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/test-task-tweets/",
+  build: {
+    outDir: "dist",
+  },
+  esbuild: {
+    jsxFactory: "h",
+    jsxFragment: "Fragment",
+  },
+  optimizeDeps: {
+    include: ["preact/hooks"],
+  },
+
+  resolve: {
+    alias: {
+      "/@": join(__dirname, "src"),
+    },
+  },
+});
